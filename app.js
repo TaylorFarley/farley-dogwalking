a5651ebc9358ae26da166a44b1ee5172b79cb658
Taylor Farley <twfarley88@gmail.com>
2020-12-05 18:57:24 -0500

    fixing routes

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client/build")));
+
+if(process.env.PORT){
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+  
 
 
 app.use('/', indexRouter);
