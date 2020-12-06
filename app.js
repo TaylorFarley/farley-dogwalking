82a96abbf32aa7174a70ec596bf8c2347f564056
Taylor Farley <twfarley88@gmail.com>
2020-12-05 19:13:48 -0500

    fix

--- app.js
+++ app.js
@@ -23,17 +23,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/appointments', appointmentsRouter);
 if(process.env.PORT){
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
   
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/appointments', appointmentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
