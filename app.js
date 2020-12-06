592de8a28d3afc504d924d9e10f203f660cbf732
Taylor Farley <twfarley88@gmail.com>
2020-12-05 20:09:59 -0500

    fix

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var appointmentsRouter = require("./routes/appointments");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var appointmentsRouter = require('./routes/appointments');
 var mongoDB = process.env.DB;
 var app = express();
-var cors = require("cors");
-app.use(cors());
 
-app.use(logger("dev"));
+// view engine setup
+
+var cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
+
+
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use("/users", usersRouter);
-app.use("/appointments", appointmentsRouter);
-
-var mongoDB = process.env;
-mongoose
-  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("CONNECTED!!!!!!!!!!!!:)");
-  });
-
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
 app.use(express.static(path.join(__dirname, "client/build")));
 
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/appointments', appointmentsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+//DB SETUP
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{console.log('CONNECTED!!!!!!!!!!!!:)')});
+var db = mongoose.connection;
+//DB SETUP
+
+
+
+app.use(express.static(path.join(__dirname, "client/build")));
 module.exports = app;
