ce12e50aef80a4c3043ceb8a2e8365352662a6ae
Taylor Farley <twfarley88@gmail.com>
2020-12-05 19:46:07 -0500

    lets go

--- app.js
+++ app.js
@@ -9,12 +9,8 @@ var usersRouter = require('./routes/users');
 var appointmentsRouter = require('./routes/appointments');
 var mongoDB = process.env.DB;
 var app = express();
-
-// view engine setup
-
 var cors = require('cors')
-
-app.use(cors()) // Use this after the variable declaration
+app.use(cors()) 
 
 
 
@@ -25,7 +21,7 @@ app.use(cookieParser());
 
 
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/appointments', appointmentsRouter);
 
@@ -36,31 +32,10 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useF
 .then(()=>{console.log('CONNECTED!!!!!!!!!!!!:)')});
 var db = mongoose.connection;
 //DB SETUP
+app.use(express.static(path.join(__dirname, "client/build")));
+   
 
-if(process.env.PORT){
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-  
-  
-  app.use((req, res, next) => {
-    next(createError(404));
-  });
-  // ssl fix 
-  app.get('*',function(req,res,next){
-    if(req.headers['x-forwarded-proto']!='https')
-      res.redirect('https://farley-dogwalking.herokuapp.com/'+req.url)
-    else
-      next() /* Continue to other routes if we're not redirecting */
-  })
-  // end of ssl fix
-  app.use(function (err, req, res, next) {
-    console.error(err.message);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
-  });
-  }
+    
 
 
 
