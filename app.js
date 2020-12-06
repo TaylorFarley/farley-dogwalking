95382b6caf71e9673dcfd0d1a1434d807565de0f
Taylor Farley <twfarley88@gmail.com>
2020-12-06 09:28:25 -0500

    heroku

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var appointmentsRouter = require('./routes/appointments');
+var appointmentRouter = require('./routes/appointments');
 var mongoDB = process.env.DB;
 var app = express();
-
-// view engine setup
 
 var cors = require('cors')
 
 app.use(cors()) // Use this after the variable declaration
-
 
 
 app.use(logger('dev'));
@@ -24,15 +21,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client/build")));
 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/appointments', appointmentsRouter);
-
+app.use('/appointments',appointmentRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 //DB SETUP
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -41,6 +37,16 @@ var db = mongoose.connection;
 //DB SETUP
 
 
+// error handler
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 module.exports = app;
