32bbfefe26d2bcac0d77a58a4a107b9328066cea
Taylor Farley <twfarley88@gmail.com>
2020-12-22 21:29:02 -0500

    working on form

--- routes/appointments.js
+++ routes/appointments.js
@@ -4,36 +4,6 @@ var router = express.Router();
 const appointment = require("../models/appointmentModel");
 let mongoose = require("mongoose");
 
-router.post("/bookAPI", async (req, res) => {
-  try {
-    let { thedate, uid } = req.body;
-
-    let timeslots = [
-      {time1: "7:00am-7:30am" },
-      {time2: "7:30am-8:00am" },
-      {time3: "8:00am-8:30am" },
-      {time4: "11:00am-11:30am"},
-      {time5: "11:30am-12:00pm"},
-      {time6: "5:00pm-5:30am"},
-    ];
-
-    const newApt = new appointment({
-      thedate,
-      timeslots,
-      uid,
-    });
-
-    appointment.create(newApt, (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.send("succesfully created");
-      }
-    });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
 
 router.post("/checkavailabletimes", async (req, res) => {
   let timeslots_default = [
@@ -51,7 +21,7 @@ router.post("/checkavailabletimes", async (req, res) => {
         console.log("No Appointments Found");
         res.send(timeslots_default)
       } else {
-        console.log("something was found");
+        console.log("Appointments Found: BELOW:");
         console.log(data[0].timeslots)
         let availabletimeslots = data[0].timeslots
         res.send(availabletimeslots);
@@ -63,8 +33,8 @@ router.post("/checkavailabletimes", async (req, res) => {
 });
 router.post("/reservation", async (req, res) => {
   let {thedate, pickedtime, email, googleId, username, address, phone} = req.body.obj
- console.log(thedate)
- console.log(pickedtime)
+//  console.log(thedate)
+//  console.log(pickedtime)
  const newApt = new appointment({
   thedate,
   timeslots: pickedtime,
@@ -81,15 +51,15 @@ router.post("/reservation", async (req, res) => {
   appointment.find({ thedate: req.body.obj.thedate }, (error, data) => {
     if (data[0] == undefined) {
       console.log("No Dates Found");
-      appointment.create(newApt, (error, data) => {
+      appointment.create(newApt, async(error, data) => {
         if (error) {
           return next(error);
         } else {
-          res.send("succesfully created");
+              res.send('successfully created')
         }
       });
     } else {
-      console.log("A Date Found");     
+       console.log("A Date Found");     
       appointment.findOneAndUpdate(
         { thedate: req.body.obj.thedate }, 
         { $push: { timeslots: req.body.obj.pickedtime } },
