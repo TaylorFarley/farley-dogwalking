e9c647437aa3ac2a7059f1e592c8729b2713b6c7
Taylor Farley <twfarley88@gmail.com>
2021-01-10 14:44:09 -0500

    fixing email

--- sendEmail.js
+++ sendEmail.js
@@ -1,31 +1,25 @@
 
-var nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const sendEmail = async(newApt)=>{
-  var transporter = nodemailer.createTransport({
-      host: "mail.privateemail.com",
-      port: 587,
-      secure: false,
-    auth: {
-      user:  process.env.AUTH,
-      pass:  process.env.PASSWORD,
-    }
-  });
   
-  var mailOptions = {
-    from: 'hi@twfmade.ca',
-    to: 'hi@twfmade.ca',
-    subject: 'New Apt Reqiest',
-    text: `${newApt.email}wants a walk at ${newApt.thedate} at ${newApt.timeslots}`
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-      console.log(newApt)
+   
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: 'hi@twfmade.ca', // Change to your recipient
+      from: 'hi@twfmade.ca', // Change to your verified sender
+      subject: 'email from contact form dogwalking',
+      text: `${newApt.email}wants a walk at ${newApt.thedate} at ${newApt.timeslots}`,
+      html: `${newApt.email}wants a walk at ${newApt.thedate} at ${newApt.timeslots}`,
     }
-  });
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+        console.error(error.response.body)
+      })
   }
 
   exports.sendEmail = sendEmail
