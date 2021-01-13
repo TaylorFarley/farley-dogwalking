b7748dbf95acdff347f872b26b202695f0f808d3
Taylor Farley <twfarley88@gmail.com>
2021-01-12 20:41:04 -0500

    captchav2

--- client/src/index.js
+++ client/src/index.js
@@ -9,14 +9,6 @@ ReactDOM.render(
   <React.StrictMode>
       <GoogleReCaptchaProvider
     reCaptchaKey="6LchJioaAAAAADlyrXnpBEQZckXkhjS4kGz6rkuC"
-    language="[optional_language]"
-    useRecaptchaNet="[optional_boolean_value]"
-    scriptProps={{
-      async: false, // optional, default to false,
-      defer: false, // optional, default to false
-      appendTo: "head", // optional, default to "head", can be "head" or "body",
-      nonce: undefined // optional, default undefined
-    }}
   >
     <App />
     </GoogleReCaptchaProvider>,
