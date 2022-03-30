b13403141938104a87b22cb9fbfb691942733086
taylor <twfarley88@gmail.com>
2022-03-30 10:26:01 -0400

    fixed background

--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -110,7 +110,7 @@ axios.post('contactform/sendEmailContact/',contactform)
                 <div className="et-l et-l--post">
                   <div className="et_builder_inner_content et_pb_gutters3">
                     <div className="et_pb_section et_pb_section_0 et_pb_section_parallax et_pb_with_background et_section_regular">
-                    <span className="et_parallax_bg_wrap"><span className="et_parallax_bg" style={{backgroundImage: 'url(images/dog-walker-10-3.png)'}} /></span>
+                    <span className="et_parallax_bg_wrap"><span className="et_parallax_bg" style={{backgroundImage: 'url(images/dog-walker-10-3-background.png)'}} /></span>
                     <div className="et_pb_row et_pb_row_0">
                       <div className="et_pb_column et_pb_column_3_5 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough">
                         <div className="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_light">
